c4fa5930636f4d54afbe29a830bd9911c1bc4f7e
andor <andor.umann@gmail.com>
2020-03-09 15:50:51 +0100

    Added new test to user

---
+++ train-user/src/test/java/hu/bme/mit/train/system/TrainUserTest.java
@@ -0,0 +1,19 @@
+package hu.bme.mit.train.system;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class TrainUserTest {
+
+    @Before
+    public void before() {
+        // TODO Add initializations
+    }
+
+    @Test
+    public void ThisIsAnExampleTestStub() {
+        // TODO Delete this and add test cases based on the issues
+    }
+}
